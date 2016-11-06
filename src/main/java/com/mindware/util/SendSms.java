bf3a59854d184f510e84e9bf65f30282b09963c7
ideat <ideatsoft@gmail.com>
2016-11-06 15:12:28 -0400

    SendMessage

--- src/main/java/com/mindware/util/SendSms.java
+++ src/main/java/com/mindware/util/SendSms.java
@@ -1,39 +1,44 @@
 package com.mindware.util;
 
-//import org.smslib.AGateway;
-//import org.smslib.IOutboundMessageNotification;
-//import org.smslib.Library;
-//import org.smslib.OutboundMessage;
-//import org.smslib.OutboundWapSIMessage;
-//import org.smslib.*;
-//import org.smslib.Service;
-//import org.smslib.modem.SerialModemGateway;
+import org.smslib.*;
+import org.smslib.modem.SerialModemGateway;
+
+import java.io.IOException;
 
 /**
  * Created by freddy on 06-11-16.
  */
 public class SendSms {
-    private void sendMessage() {
-//        OutboundNotification outboundNotification = new OutboundNotification();
-//
-//        SerialModemGateway gateway = new SerialModemGateway("modem.com9",
-//                "COM9", 9600, "Huawei", "E303");
-//        gateway.setInbound(false);
-//        gateway.setOutbound(true);
-//        // gateway.setSimPin("");
-//        Service.getInstance().setOutboundMessageNotification(outboundNotification);
-//        Service.getInstance().addGateway(gateway);
-//        Service.getInstance().startService();
-//
-//        // Send a message synchronously.
-//        OutboundMessage msg = new OutboundMessage("+59160700381",
-//                "Duque Chevas");
-//
-////    Service service = Service.getInstance();
-//        Service.getInstance().sendMessage(msg);
-//
-////        Service.getInstance().stopService();
+    private void sendMessage(String mensaje, String celular) throws SMSLibException, InterruptedException, IOException {
+        OutboundNotification outboundNotification = new OutboundNotification();
+
+        SerialModemGateway gateway = new SerialModemGateway("modem.com9",
+                "COM9", 9600, "Huawei", "E303");
+        gateway.setInbound(false);
+        gateway.setOutbound(true);
+        // gateway.setSimPin("");
+        Service.getInstance().setOutboundMessageNotification(outboundNotification);
+        Service.getInstance().addGateway(gateway);
+        Service.getInstance().startService();
+
+        // Send a message synchronously. "+59160700381"
+        OutboundMessage msg = new OutboundMessage(celular,
+                mensaje);
+
+//    Service service = Service.getInstance();
+        Service.getInstance().sendMessage(msg);
+
+//        Service.getInstance().stopService();
 
 
+    }
+
+    public class OutboundNotification implements IOutboundMessageNotification
+    {
+        public void process(AGateway gateway, OutboundMessage msg)
+        {
+            System.out.println("Outbound handler called from Gateway: " + gateway.getGatewayId());
+            System.out.println(msg);
+        }
     }
 }
