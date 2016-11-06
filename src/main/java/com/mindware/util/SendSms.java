b2624bfc5b73781cbb1b5b852ce4987de73ba1e9
ideat <ideatsoft@gmail.com>
2016-11-06 14:02:11 -0400

    Subida proyecto

--- src/main/java/com/mindware/util/SendSms.java
+++ src/main/java/com/mindware/util/SendSms.java
@@ -1,6 +1,5 @@
 package com.mindware.util;
-import java.net.URL;
-
+/*
 import org.smslib.AGateway;
 import org.smslib.IOutboundMessageNotification;
 import org.smslib.Library;
@@ -8,31 +7,31 @@ import org.smslib.OutboundMessage;
 import org.smslib.OutboundWapSIMessage;
 import org.smslib.Service;
 import org.smslib.modem.SerialModemGateway;
-
+*/
 /**
  * Created by freddy on 06-11-16.
  */
 public class SendSms {
     private void sendMessage() {
-        OutboundNotification outboundNotification = new OutboundNotification();
-
-        SerialModemGateway gateway = new SerialModemGateway("modem.com9",
-                "COM9", 9600, "Huawei", "E303");
-        gateway.setInbound(false);
-        gateway.setOutbound(true);
-        // gateway.setSimPin("");
-        Service.getInstance().setOutboundMessageNotification(outboundNotification);
-        Service.getInstance().addGateway(gateway);
-        Service.getInstance().startService();
-
-        // Send a message synchronously.
-        OutboundMessage msg = new OutboundMessage("+59160700381",
-                "Duque Chevas");
-
-//    Service srvice = Service.getInstance();
-        Service.getInstance().sendMessage(msg);
-
-        Service.getInstance().stopService();
+//        OutboundNotification outboundNotification = new OutboundNotification();
+//
+//        SerialModemGateway gateway = new SerialModemGateway("modem.com9",
+//                "COM9", 9600, "Huawei", "E303");
+//        gateway.setInbound(false);
+//        gateway.setOutbound(true);
+//        // gateway.setSimPin("");
+//        Service.getInstance().setOutboundMessageNotification(outboundNotification);
+//        Service.getInstance().addGateway(gateway);
+//        Service.getInstance().startService();
+//
+//        // Send a message synchronously.
+//        OutboundMessage msg = new OutboundMessage("+59160700381",
+//                "Duque Chevas");
+//
+////    Service srvice = Service.getInstance();
+//        Service.getInstance().sendMessage(msg);
+//
+//        Service.getInstance().stopService();
 
 
     }
