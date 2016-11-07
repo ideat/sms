f29340eb4b797a6d4ab956245935b8547ee419ea
freddy <freddy@WIN-598FQEH81QB>
2016-11-07 01:17:39 -0400

    smslib

--- src/main/java/com/mindware/util/SendSms.java
+++ src/main/java/com/mindware/util/SendSms.java
@@ -1,7 +1,14 @@
 package com.mindware.util;
 
-import org.smslib.*;
+import org.smslib.AGateway;
+import org.smslib.IOutboundMessageNotification;
+import org.smslib.Library;
+import org.smslib.OutboundMessage;
+import org.smslib.OutboundWapSIMessage;
+import org.smslib.Service;
 import org.smslib.modem.SerialModemGateway;
+
+
 
 import java.io.IOException;
 
@@ -9,36 +16,55 @@ import java.io.IOException;
  * Created by freddy on 06-11-16.
  */
 public class SendSms {
-    private void sendMessage(String mensaje, String celular) throws SMSLibException, InterruptedException, IOException {
-        OutboundNotification outboundNotification = new OutboundNotification();
 
-        SerialModemGateway gateway = new SerialModemGateway("modem.com9",
-                "COM9", 9600, "Huawei", "E303");
-        gateway.setInbound(false);
-        gateway.setOutbound(true);
-        // gateway.setSimPin("");
-        Service.getInstance().setOutboundMessageNotification(outboundNotification);
-        Service.getInstance().addGateway(gateway);
-        Service.getInstance().startService();
+	public void sendMessage() throws Exception {
+	    OutboundNotification outboundNotification = new OutboundNotification();
+	    System.out.println("Sample of Send message from a serial gsm modem.");
+	    System.out.println(Library.getLibraryDescription());
+	    System.out.println("Version: " + Library.getLibraryVersion());
+	    SerialModemGateway gateway = new SerialModemGateway("modem.com6", "COM6", 9600, "Huawei", "E303");
+	   
+	    gateway.setInbound(false);
+	    gateway.setOutbound(true);
 
-        // Send a message synchronously. "+59160700381"
-        OutboundMessage msg = new OutboundMessage(celular,
-                mensaje);
+	    Service.getInstance().setOutboundMessageNotification(outboundNotification);
+	    Service.getInstance().addGateway(gateway);
+	    Service.getInstance().startService();
+	    
+	    System.out.println();
+	    System.out.println("Modem Information:");
+	    System.out.println("  Manufacturer: " + gateway.getManufacturer());
+	    System.out.println("  Model: " + gateway.getModel());
+	    System.out.println("  Serial No: " + gateway.getSerialNo());
+	    System.out.println("  SIM IMSI: " + gateway.getImsi());
+	    System.out.println("  Signal Level: " + gateway.getSignalLevel()
+	            + " dBm");
+	    System.out.println("  Battery Level: " + gateway.getBatteryLevel()
+	            + "%");
 
-//    Service service = Service.getInstance();
-        Service.getInstance().sendMessage(msg);
+	    // Send a message synchronously.
+	    OutboundMessage msg = new OutboundMessage("+59160700381",
+	            "Duque Chevas");
 
-//        Service.getInstance().stopService();
+//	    Service srvice = Service.getInstance();
+	    Service.getInstance().sendMessage(msg);
+	    System.out.println(msg);
+	    // Or, send out a WAP SI message.
+	  //  OutboundWapSIMessage wapMsg = new OutboundWapSIMessage("+59160700381",
+	  //          new URL("http://stackoverflow.com/"),
+	  //          "WAP test: sample message from StackOverflow!");
+	    // gateway.setFrom("chandpriyankara");
+	    // wapMsg.setFrom("chandpriyankara");
+	    //srvice.queueMessage(wapMsg);
 
+	    Service.getInstance().stopService();
+	}
 
-    }
-
-    public class OutboundNotification implements IOutboundMessageNotification
-    {
-        public void process(AGateway gateway, OutboundMessage msg)
-        {
-            System.out.println("Outbound handler called from Gateway: " + gateway.getGatewayId());
-            System.out.println(msg);
-        }
-    }
+	public class OutboundNotification implements IOutboundMessageNotification {
+	    public void process(AGateway gateway, OutboundMessage msg) {
+	        System.out.println("Outbound handler called from Gateway: "
+	                + gateway.getGatewayId());
+	        System.out.println(msg);
+	    }
+	}
 }
